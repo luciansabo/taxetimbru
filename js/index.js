6219eef1f0bf3f14bb602bd30bb428d2ced9ab04
Lucian Sabo <luciansabo@gmail.com>
2015-11-06 19:29:26 +0200

    using window.open

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ var taxInfo = new Object();
 var jqmReady = $.Deferred();
 var pgReady = $.Deferred();
 
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
+
 // jqm ready
 $(document).bind("mobileinit", jqmReady.resolve);
 
@@ -37,11 +42,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        var ref = window.open('http://apache.org', '_blank', 'location=yes');
-        ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-        ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-        ref.addEventListener('exit', function(event) { alert(event.type); });
         //app.receivedEvent('deviceready');
         this.jqmSetup();
         this.bootstrap();
